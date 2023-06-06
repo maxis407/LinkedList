5acb0de2b496a9087cdce44d62436d34c817bc65
maxis407 <108719076+maxis407@users.noreply.github.com>
2023-06-06 12:18:43 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        
+        
+        
+         int[] values = {5, 2, 8, 1, 3, 7, 9};
+        BinaryThree three = new BinaryThree(values);
+
+
+
+        three.delete(2);
 
 
 
